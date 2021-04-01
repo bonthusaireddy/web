38f33dfced3e2c819e2cc47a07965c7873955552
bonthusaireddy <52176773+bonthusaireddy@users.noreply.github.com>
2021-04-02 00:47:23 +0530

    Files added
    Files added

---
+++ script/slider.js
@@ -0,0 +1,19 @@
+// Swiper function is for slider
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true, // for slider loop
+});
+
